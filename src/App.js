bf953d4c7a26bb42f611f3f7fc6eaad86915ba27
Varma <sangaraju.varma1@gmail.com>
2021-01-19 14:38:55 -0500

    base app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Hello from './sayHello';
+
+import Tweet from"./Tweet";
+
+function App(){
+
+  // const[isRed, setRed]= useState(false);
+  // const[count,setcount]= useState(0);
+
+  // const increment = () =>{
+  //   setcount(count+1);
+  //   setRed(!isRed);
+
+    const[users, setUsers] = useState([
+      {name:"varma", message:"Hi!"},
+      {name:"Sang", message:"Hello!"},
+      {name:"Nag", message:"There!"}
+    ]
+
+    );
+
+  
+
+  return(
+    <div className="app">
+   
+      {/* <Tweet name="Dev Ed" message="hi" />
+      <Tweet name="John Snow" message="there?" />
+      <Tweet name="Traversy Media" message="100K" />
+      <Tweet name="Mosh" message="Media"/> */}
+
+      {/* <h1 className={isRed ? "red": "" }>Change my color:</h1>
+
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1> */}
+
+      { users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))
+
+      }
+     
     </div>
   );
 }
